a8f4ab6652c5b711c19f181dd658e70e03595159
nicolasvogel1 <nicolasvogel1@hotmail.com>
2026-03-11 10:51:56 -0300

    Agrego formulario de busqueda y correcciones en views

--- miweb/blog/urls.py
+++ miweb/blog/urls.py
@@ -1,10 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import inicio, crear_autor, crear_post, buscar_post
 
 urlpatterns = [
-
-    path('admin/', admin.site.urls),
-
-    path('', include('blog.urls')),
-
+    path('', inicio, name="inicio"),
+    path('autor/', crear_autor, name="crear_autor"),
+    path('post/', crear_post, name="crear_post"),
+    path('buscar/', buscar_post, name="buscar_post"),
 ]
--- miweb/blog/views.py
+++ miweb/blog/views.py
@@ -1,10 +1,10 @@
+from .models import Post
 from django.shortcuts import render
-from .forms import AutorFormulario
-from .models import Autor
+from .forms import AutorFormulario, PostFormulario
+from .models import Autor, Post
 
 
 def inicio(request):
-
     return render(request, "blog/inicio.html")
 
 
@@ -20,6 +20,7 @@ def crear_autor(request):
 
             autor = Autor(
                 nombre=data["nombre"],
+                apellido=data["apellido"],
                 email=data["email"]
             )
 
@@ -32,3 +33,42 @@ def crear_autor(request):
         form = AutorFormulario()
 
     return render(request, "blog/form_autor.html", {"form": form})
+
+
+def crear_post(request):
+
+    if request.method == "POST":
+
+        form = PostFormulario(request.POST)
+
+        if form.is_valid():
+
+            data = form.cleaned_data
+
+            post = Post(
+                titulo=data["titulo"],
+                contenido=data["contenido"]
+            )
+
+            post.save()
+
+            return render(request, "blog/inicio.html")
+
+    else:
+
+        form = PostFormulario()
+
+    return render(request, "blog/form_post.html", {"form": form})
+
+
+def buscar_post(request):
+
+    if request.GET.get("titulo"):
+
+        titulo = request.GET.get("titulo")
+
+        posts = Post.objects.filter(titulo__icontains=titulo)
+
+        return render(request, "blog/resultados_busqueda.html", {"posts": posts})
+
+    return render(request, "blog/buscar_post.html")
